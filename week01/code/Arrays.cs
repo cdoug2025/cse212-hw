61181540a78c3150ebddd4f59818b523dbae563c
cdoug2025 <chris5617@hotmail.com>
2026-01-10 21:06:33 -0500

    Completed Assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create an array with capacity 'length'
+        // In a for loop, iterate through the loop 'length' times, with the itertion number starting at zero and doubling as an index number
+        // With 'multiple' starting at 0, in each iteration, add 'number' to 'multiple',
+        // and put 'multiple' in the iteration or index number of 'result'.
+        // Return result.
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+        double multiple = 0;
+        for (int index = 0; index < length; index++)
+        {
+            multiple += number;
+            result[index] = multiple;
+        }
+
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +34,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        // GetRange where the index is equal to 'length' - 'amount', and the count is equal to 'amount'.
+        // AddRange this to a new list.
+        // GetRange where the index is equal to 0, and the count is equal to 'length' - 'amount'.
+        // AddRange the result to the new list.
+        // Clear 'data'.
+        // Add new list into 'data'.
+
+        List<int> shiftedList = new();
+        shiftedList.AddRange(data.GetRange(data.Count - amount, amount));
+        shiftedList.AddRange(data.GetRange(0, data.Count - amount));
+        data.Clear();
+        data.AddRange(shiftedList);
     }
 }
