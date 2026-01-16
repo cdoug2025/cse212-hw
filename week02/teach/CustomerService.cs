57b7e76ee72df24a669c324b6eeffcd1960c1b59
cdoug2025 <chris5617@hotmail.com>
2026-01-15 21:22:00 -0500

    Completed Week 2 Team Activity

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -1,4 +1,7 @@
-﻿/// <summary>
+﻿using System.Runtime;
+using System.Runtime.InteropServices.JavaScript;
+
+/// <summary>
 /// Maintain a Customer Service Queue.  Allows new customers to be 
 /// added and allows customers to be serviced.
 /// </summary>
@@ -11,20 +14,64 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Input is 0
+        // Expected Result: Max size = 10
         Console.WriteLine("Test 1");
+        var cs = new CustomerService(0);
+        Console.WriteLine(cs);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Add a customer to the queue
+        // Expected Result: A queue with one Customer
         Console.WriteLine("Test 2");
+        cs = new CustomerService(5);
+        cs.AddNewCustomer();
+        Console.WriteLine(cs);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None
+
+        Console.WriteLine("=================");
+
+        // Test 3
+        // Scenario: Ensure an error message is sent when the queue overflows
+        // Expected Result: A message saying the queue is full
+        Console.WriteLine("Test 3");
+        cs = new CustomerService(1);
+        cs.AddNewCustomer();
+        cs.AddNewCustomer();
+        Console.WriteLine(cs);
+
+        // Defect(s) Found: > instead of >=
+
+        Console.WriteLine("=================");
+
+        // Test 4
+        // Scenario: Testing the ServeCustomer function
+        // Expected Result: Dequeue the next customer and display the details
+        Console.WriteLine("Test 4");
+        cs = new CustomerService(5);
+        cs.AddNewCustomer();
+        cs.AddNewCustomer();
+        cs.ServeCustomer();
+        Console.WriteLine(cs);
+
+        // Defect(s) Found: Dequeued before showing information of first customer in queue so the wrong info was displayed
+
+        Console.WriteLine("=================");
+
+        // Test 5
+        // Scenario: Testing the ServeCustomer function when the queue is empty
+        // Expected Result: An error message
+        Console.WriteLine("Test 5");
+        cs = new CustomerService(5);
+        cs.ServeCustomer();
+        Console.WriteLine(cs);
+
+        // Defect(s) Found: No error handling
 
         Console.WriteLine("=================");
 
@@ -67,7 +114,7 @@ public class CustomerService {
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -88,8 +135,13 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
+        if (_queue.Count == 0) {
+            Console.WriteLine("No Customers in Queue.");
+            return;
+        }
+
         var customer = _queue[0];
+        _queue.RemoveAt(0);
         Console.WriteLine(customer);
     }
 
