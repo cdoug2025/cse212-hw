048a5d19f446d02769fe8c9439598ed5e815498f
cdoug2025 <chris5617@hotmail.com>
2026-01-17 15:20:16 -0500

    Completed Week 2 Coding Assignment

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,55 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following values and priority: priorityItem1 (4), priorityItem2 (6), priorityItem3 (1), priorityItem4 (6) and
+    // run until the queue is empty
+    // Expected Result: priorityItem2, priorityItem4, priorityItem1, priorityItem3
+    // Defect(s) Found: Did not remove priorityItem2 from the queue, wasn't checking the last item in queue, was checking >= for highest priority
+    // when only > should be checked
     public void TestPriorityQueue_1()
     {
+        string[] expectedResult = ["priorityItem2", "priorityItem4", "priorityItem1", "priorityItem3"];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("priorityItem1", 4);
+        priorityQueue.Enqueue("priorityItem2", 6);
+        priorityQueue.Enqueue("priorityItem3", 1);
+        priorityQueue.Enqueue("priorityItem4", 6);
+
+        int index = 0;
+        while (index < 4)
+        {
+            string currentItem = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[index], currentItem);
+            index++;
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Attempt to dequeue from an empty queue
+    // Expected Result: Exception should be thrown with appropriate error message
+    // Defect(s) Found: None
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException exception)
+        {
+            Assert.AreEqual("The queue is empty.", exception.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception exception)
+        {
+            Assert.Fail(string.Format("Unexpected exception of type {0}: {1}", exception.GetType(), exception.Message));
+        }
     }
 
     // Add more test cases as needed below.
