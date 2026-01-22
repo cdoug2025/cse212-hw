496572a464eca448ef095e6411602a397b446862
cdoug2025 <chris5617@hotmail.com>
2026-01-22 15:13:17 -0500

    Completed Week 3 Learning Assignments

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,7 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> set1 = new(data);
+        return data.Length - set1.Count;
     }
 }
